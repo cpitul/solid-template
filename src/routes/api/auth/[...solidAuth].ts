3249d6d4d9e007f44cca5233c40635e35a2d9ed7
KIPRI <hi@kipri.dev>
2023-02-19 19:18:32 +0200

    created utility auth file

--- src/routes/api/auth/[...solidAuth].ts
+++ src/routes/api/auth/[...solidAuth].ts
@@ -1,44 +1,4 @@
-import { SolidAuth, type SolidAuthConfig } from "@auth/solid-start";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import type { Adapter } from "@auth/core/adapters";
-import { serverEnv } from "~/env/server";
-import { prisma } from "~/server/db/client";
-
-export const authOpts: SolidAuthConfig = {
-    callbacks: {
-        signIn() {
-            const isAllowedToSignIn = !void 0;
-            // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-            if (!isAllowedToSignIn) {
-                return false;
-            }
-            return true;
-        },
-        session({ session, user }) {
-            if (session.user) {
-                // @ts-expect-error xD!
-                session.user.id = user.id;
-            }
-
-            return session;
-        },
-    },
-    adapter: PrismaAdapter(prisma) as Adapter,
-    providers: [
-        // GitHub({
-        //     clientId: serverEnv.GITHUB_ID,
-        //     clientSecret: serverEnv.GITHUB_SECRET
-        // }),
-        // Discord({
-        //     clientId: serverEnv.DISCORD_ID,
-        //     clientSecret: serverEnv.DISCORD_SECRET,
-        // }),
-    ],
-    session: {
-        strategy: "database",
-        generateSessionToken: () => crypto.randomUUID(),
-    },
-    debug: serverEnv.NODE_ENV !== "production",
-};
+import { SolidAuth } from "@auth/solid-start";
+import { authOpts } from "~/utils/auth";
 
 export const { GET, POST } = SolidAuth(authOpts);
